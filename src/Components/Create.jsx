bdf8da41a2d0768e3ef8f90efab2b691fd47d2df
minthukha-coding <minthukha1239788@gmail.com>
2024-01-05 11:57:21 +0630

    Complete

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [content, setContent] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isLoading, setisLoading] = useState(false);
 
@@ -11,17 +12,17 @@ const Create = () => {
 
   const sumbitHandaler = (e) => {
     e.preventDefault();
-    const blog = { title, body, author };
+    const blog = { Blog_Title, Blog_Author, Blog_Content };
 
     setisLoading(true);
-    fetch("http://localhost:8000/blogs", {
+    fetch("https://localhost:7241/api/BlogAdoDotNet", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog),
     }).then(() => {
       console.log("New blog add");
       setisLoading(false);
-      navigate('/');
+      navigate("/");
     });
   };
 
@@ -33,23 +34,40 @@ const Create = () => {
         <input
           type="text"
           required
-          value={title}
+          value={Blog_Title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
           required
           value={body}
-          onChange={(e) => setBody(e.target.value)}></textarea>
-        <label>Blog author:</label>
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog_Author:</label>
+        <input
+          type="text"
+          required
+          value={Blog_Author}
+          onChange={(e) => setContent(e.target.value)}
+        />
+               
+         <label>Blog Content:</label>
+        <input
+          type="text"
+          required
+          value={Blog_Content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        {/* <label>Blog author:</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
-        </select>
-            {!isLoading && <button>Add Blog</button>}
-            {isLoading && <button>Addiing...</button>}
+        </select> */}
+        {!isLoading && <button>Add Blog</button>}
+        {isLoading && <button>Addiing...</button>}
       </form>
     </div>
-  )};
+  );
+};
 
 export default Create;
